6577f4f4d471fb140d49a1428395f7ddbc816fca
AleksandrOnis <sashaonis@gmail.com>
2021-10-08 11:37:10 +0300

    Added notiflix-3.1.0
    Install and connected

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@
 import filmСard from './templates/filmСard.hbs';
 import filmDescription from './templates/filmDescription.hbs';
 import filmsApiService from './js/apiService.js';
+import Notiflix from 'notiflix';
