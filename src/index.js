92832ab1c3bd22efeb3b6be24ed0e644ab9e28cd
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-10-09 00:03:28 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './sass/main.scss';
+import filmСard from './templates/filmСard.hbs';
+import filmDescription from './templates/filmDescription.hbs';
+import filmsApiService from './js/apiService.js';
+import Notiflix from 'notiflix';
