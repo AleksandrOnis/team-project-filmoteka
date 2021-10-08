5ad34c66a1022675e41f18395093f877da72d61f
AleksandrOnis <sashaonis@gmail.com>
2021-10-08 11:20:32 +0300

    fix
    Added file - filmDescription.hbs + connected, fix container.scss

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // import './sass/main.scss';
 import filmСard from './templates/filmСard.hbs';
+import filmDescription from './templates/filmDescription.hbs';
 import filmsApiService from './js/apiService.js';
