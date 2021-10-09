094b47135861492bec4fa992ffce9c12c2dbf716
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2021-10-09 17:04:27 +0300

    fix bugs API + genreList

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -3,18 +3,28 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 const apiKey = `97183b54cab806dd382058e86706ceb2`;
 
+//https://api.themoviedb.org/3/genre/movie/list?api_key=97183b54cab806dd382058e86706ceb2&language=en-US
+
+
+//Объект со списком жанров
+ export const genreList = {
+    getGenreList() {
+        return axios.get(`genre/movie/list?api_key=${apiKey}&language=en-US`).then(response=>({list: response.data.genres}))
+    }
+};
+genreList.getGenreList();
+//-//
+
 export const filmsApiService = {
   popularMovie() {
     return axios
       .get(`trending/movie/day?api_key=${apiKey}`)
       .then(response =>
-        console.log({ results: response.data.results, total: response.data.total_results }),
+        ({ results: response.data.results, total: response.data.total_results }),
       );
   },
 };
-
 filmsApiService.popularMovie();
-
 
 
 export const searchFilmsApiService = {
@@ -33,8 +43,8 @@ export const searchFilmsApiService = {
     this.page += 1;
   },
 
-  incrementPage() {
-    this.page += 1;
+  resetPage() {
+    this.page = 1;
   },
 
   getQuery() {
@@ -45,7 +55,6 @@ export const searchFilmsApiService = {
     this.searchQuery = newQuery;
   },
 };
-
 searchFilmsApiService.searchMovie();
 
 //const PIC_URL = `https://image.tmdb.org/t/p/w500`;
