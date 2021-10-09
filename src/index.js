af7e04ba315386e277468d6c78a534bac8aa65d9
AleksandrOnis <80990738+AleksandrOnis@users.noreply.github.com>
2021-10-09 14:12:58 +0000

    Deploying to gh-pages from @ AleksandrOnis/team-project-filmoteka@43b65c70c3f102de081758d8be33b2da65d2f2c5 🚀

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './sass/main.scss';
-import filmСard from './templates/filmСard.hbs';
-import filmDescription from './templates/filmDescription.hbs';
-import filmsApiService from './js/apiService.js';
-import Notiflix from 'notiflix';
