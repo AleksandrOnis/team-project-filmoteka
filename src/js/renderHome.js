7f2f64556969764a85308713db5c3eddbb95a90d
MarharytaBoichenko <boichenkomv@gmail.com>
2021-10-10 00:14:37 +0300

    add  films rating to  card

--- src/js/renderHome.js
+++ src/js/renderHome.js
@@ -1,8 +1,13 @@
 import filmCard from "../templates/filmСard.hbs";
+// import filmCardLib from '../templates/filmcard-lib.hbs';
+
 import axios from 'axios';
 import { filmsApiService } from '../js/apiService';
 console.log(filmsApiService);
+import { genreList } from '../js/apiService';
+console.log(genreList.getGenreList());
 
+let list = [];
 
 const galleryEl = document.querySelector('.gallery');
 console.log(galleryEl);
@@ -11,8 +16,7 @@ console.log(galleryEl);
 
 filmsApiService.popularMovie().then((data) => {
     const { results } = data;
-        let imgUrl = `https://image.tmdb.org/t/p/original${data.results[1].poster_path}`;
-
+        // let imgUrl = `https://image.tmdb.org/t/p/original${data.results[1].poster_path}`;
     const newResults = getData(results)
     console.log(newResults);
     renderImageCard(newResults)
@@ -29,15 +33,24 @@ function renderImageCard(cards) {
 
 function getData(arr) {
     return arr.map(el => {
+        // год корректируем из  даты 
         if (el.release_date) {
             el.release_date = el.release_date.slice(0, 4)
         }
         if (el.first_air_date) {
             el.release_date = el.first_air_date.slice(0, 4)
         }
+
+        //название корректируем если нет title
         if (!el.title) {
             el.title = el.name;
         }
+        //рейтінг корректируем с точкой
+        el.vote_average = String(el.vote_average).padEnd(3, '.0');
+        console.log(el.vote_average);
+ 
+        
+console.log(el);
         return el;
     }
     )
