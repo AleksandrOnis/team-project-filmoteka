9acd8d0d9c212b6592c286537bc50baa4a60371e
MarharytaBoichenko <boichenkomv@gmail.com>
2021-10-11 09:00:38 +0300

    add

--- src/js/renderHome.js
+++ src/js/renderHome.js
@@ -5,21 +5,52 @@ import axios from 'axios';
 import { filmsApiService } from '../js/apiService';
 console.log(filmsApiService);
 import { genreList } from '../js/apiService';
-console.log(genreList.getGenreList());
+import genres from "../data/genres.json";
+console.log(genres);
 
-let list = [];
+// console.log(genreList.getGenreList());
+
+// genreList.getGenreList().then(({ list }) => {
+//     console.log(list);
+//     return list;
+// })
+
 
 const galleryEl = document.querySelector('.gallery');
 console.log(galleryEl);
 
 ///по данным из запроса  создаем галерею
 
+// function replaceFilmId(array) {
+//     array.forEach(object => {
+//     if (object.genre_ids) {
+//       object.genre_ids.forEach((idGenre, indexGenre) => {
+//         genres.forEach(objectNames => {
+//           if (objectNames.id === idGenre) {
+//               object.genre_ids.splice(indexGenre, 1, objectNames['name']);
+//                console.log(object.genre_ids);
+//           }
+//         });
+//       });
+//     } else {
+//       object.genre_ids = '';
+//     }
+       
+//    });
+//     // console.log(arrayId);
+//     // return arrayId;
+// }
+
 filmsApiService.popularMovie().then((data) => {
-    const { results } = data;
+  
+          const { results } = data;
         // let imgUrl = `https://image.tmdb.org/t/p/original${data.results[1].poster_path}`;
+
     const newResults = getData(results)
-    console.log(newResults);
-    renderImageCard(newResults)
+        console.log(newResults);
+    renderImageCard(newResults);
+
+  
 });
 
 ////добавляем разметку  на страницу
@@ -32,7 +63,23 @@ function renderImageCard(cards) {
 
 
 function getData(arr) {
-    return arr.map(el => {
+    
+ return arr.map(el => {
+    const filteredGenres = genres.filter(genre => el.genre_ids.includes(genre.id));
+    console.log('filteredGenres: ', filteredGenres);
+
+    const mapedGenres = filteredGenres.map(({ name }) => name);
+    console.log('mapedGenres: ', mapedGenres);
+
+    let slicedMapedGenres = [];
+
+    if (mapedGenres.length < 3) {
+      el.genre_ids = mapedGenres;
+    } else {
+      el.genre_ids = mapedGenres.slice(0, 2);
+    }
+console.log(el.genre_ids);
+ 
         // год корректируем из  даты 
         if (el.release_date) {
             el.release_date = el.release_date.slice(0, 4)
@@ -47,34 +94,7 @@ function getData(arr) {
         }
         //рейтінг корректируем с точкой
         el.vote_average = String(el.vote_average).padEnd(3, '.0');
-        console.log(el.vote_average);
- 
-        const genresArr = []
-
-
-        /////на основе полученных  из  запроса жанров  
-        genreList.getGenreList()
-            .then(list => {
-                console.log(list.list);
-                // const genresArr = []
-                console.log(el.genre_ids); 
-///перебираем массив  с  жанрами ,  НО не  получается получить  в  итоге массив  с 
-                ///именами жанров  чтобы  подставить его  в  фильм вместо  массива с цифрами жанров
-                    for (let i = 0; i <= list.list.length; i += 1) {
-                        console.log(list.list[i]);
-                        console.log(list.list[i].id);
-                        if (el.genre_ids.includes(list.list[i].id)) {
-                            genresArr.push(list.list[i].name)
-                            console.log(genresArr);
-                            // el.genre_ids = genresArr;
-                        }
-                        //  el.genre_ids = genresArr;
-                        console.log(genresArr);
-                    }
-                     console.log(genresArr);
-                    return genresArr;
-                
-                })
+        // console.log(el.vote_average);
         
                 console.log(el);
                 return el;
@@ -82,17 +102,3 @@ function getData(arr) {
             )
     }
 
-     
-
-
-// function getGenreById(genreId) {
-//   genreList.getGenreList().then(res => {
-//     const data = res.list;
-//     data.forEach(el => {
-//       console.log(el);
-//       if (el.id === genreId) {
-//         genreId = el.name;
-//         console.log(genreId);
-//       }
-//     });
-//   });
