f6c2bd7e9049ab5ab0333abbb591120265159296
MarharytaBoichenko <boichenkomv@gmail.com>
2021-10-12 21:44:57 +0300

    save

--- src/js/modalFooter.js
+++ src/js/modalFooter.js
@@ -5,17 +5,52 @@ const linkAccess = document.querySelector('.footer__link');
 const modalAccess = document.querySelector('.footer__modal');
 const closeFooterModal = document.querySelector('.footer__button');
 
+const instance = basicLightbox.create(document.querySelector('.footer__template'));
+
+
 function modalFooter(e) {
   console.log(e.target.nodeName);
-  if (e.target.nodeName === 'A') {
-    const instance = basicLightbox.create(document.querySelector('.footer__template'));
-
-    instance.show();
-  } else {
+  if (!e.target.nodeName === 'A') {
     return;
   }
+    // const instance = basicLightbox.create(document.querySelector('.footer__template'));
+
+  instance.show();
+  window.addEventListener('keydown', onEscapeClose);
+
+    // window.addEventListener('keydown', function closeModal(e){
+    //   if (e.code === 'Escape') {
+    //     console.log(e.code);
+    //     instance.close()
+    //   }
+    // })
+  } 
+
+
+// window.removeEventListener('keydown', (e) => {
+//   if (e.code === 'Escape') {
+//     console.log(e.code);
+//     instance.close()
+//   }
+// })
+function removeListener() {
+  window.removeEventListener('keydown', closeModal);
 }
-footerAccess.addEventListener('click', modalFooter);
+// window.addEventListener('keydown', closeModal);
+function onEscapeClose(e) {
+  console.log(e);
+   if (e.code === 'Escape') {
+    console.log(e.code);
+     instance.close()
+     removeListener()
+  }
+}
+// function onBtnClose() {
+//    instance.close()
+//      removeListener()
+// }
+
+// footerAccess.addEventListener('click', modalFooter);
 
 // closeFooterModal.addEventListener('click', closeModal);
 // window.addEventListener('keydown', instance.close());
