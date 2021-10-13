b3ac8517993c26f4ffb6362fec52f3ec7512146d
AleksandrOnis <sashaonis@gmail.com>
2021-10-13 15:18:26 +0300

    м11

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -8,7 +8,7 @@ const options = {
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
   template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    page: '<a href="#" class="tui-page-btn tui-page-btn-{{page}}">{{page}}</a>',
     currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
     moveButton:
       // (type-first, previous, next, last)
@@ -27,13 +27,23 @@ const options = {
   },
 };
 
-export default function pagination(totalResults, fn) {
+export default function pagination(totalResults, onCreateCards) {
   const pagination = new Pagination('pagination', { totalItems: totalResults, ...options });
-
-  document.querySelector('.tui-last').textContent = `${Math.ceil(totalResults / 20)}`;
-  document.querySelector('.tui-first').textContent = `1`;
+  const Refs = {
+    paginationConteiner: document.querySelector('#pagination'),
+    tuiFirst: document.querySelector('.tui-first'),
+    tuiLast: document.querySelector('.tui-last'),
+    tuiPrev: document.querySelector('.tui-prev'),
+    tuiNext: document.querySelector('.tui-next'),
+    tuiMorePrev: document.querySelector('.tui-prev-is-ellip'),
+    tuiMoreNext: document.querySelector('.tui-next-is-ellip'),
+  };
+  Refs.tuiLast.insertAdjacentElement('afterend', Refs.tuiNext);
+  Refs.tuiFirst.insertAdjacentElement('beforebegin', Refs.tuiPrev);
+  Refs.tuiLast.textContent = `${Math.ceil(totalResults / 20)}`;
+  Refs.tuiFirst.textContent = `1`;
 
   pagination.on('beforeMove', event => {
-    fn(event.page);
+    onCreateCards(event.page);
   });
 }
--- src/js/spiner.js
+++ src/js/spiner.js
@@ -1,21 +1,17 @@
-const spiner = document.querySelector('.spiner')
-console.log(spiner);
-  
+const spiner = document.querySelector('.spiner');
+
 export function showSpiner() {
-    const spiner = document.querySelector('.spiner')
-// const spiner = document.querySelector('.spiner');
-console.log(spiner);
-    console.log(`вкл  спин`);
-    spiner.classList.remove('hidden')
+  const spiner = document.querySelector('.spiner');
+  // const spiner = document.querySelector('.spiner');
+  spiner.classList.remove('hidden');
 }
 
 export function hideSpiner() {
-     console.log(`ВЫКЛ  спин`);
-    setTimeout(function () {
-        if (!spiner.classList.contains('hidden')) {
-            spiner.classList.add('hidden')
-        }
-    }, 500)
+  setTimeout(function () {
+    if (!spiner.classList.contains('hidden')) {
+      spiner.classList.add('hidden');
+    }
+  }, 500);
 }
 
-document.body.onload = hideSpiner()
+document.body.onload = hideSpiner();
