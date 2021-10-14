06f91d17cf1204cde109fdd87311af01c964466b
eche-unicorn <d.anasteysha@gmail.com>
2021-10-14 18:09:14 +0300

    footer fixed

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,4 +2,8 @@ export default {
   mainHTML: document.querySelector('main'),
   myLibraryLink: document.querySelector('.navigation__library'),
   homeLink: document.querySelector('.navigation__home'),
+  footerAccess: document.querySelector('.footer'),
+  footerLinkAccess: document.querySelector('.footer__link'),
+  footerModalAccess: document.querySelector('.footer__modal'),
+  footerCloseBtn: document.querySelector('.footer__button'),
 };
