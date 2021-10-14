10102049203bba27d799e5e275f0315efd014fa6
eche-unicorn <d.anasteysha@gmail.com>
2021-10-14 17:37:39 +0300

    transfer finished

---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  mainHTML: document.querySelector('main'),
+  myLibraryLink: document.querySelector('.navigation__library'),
+  homeLink: document.querySelector('.navigation__home'),
+};
--- src/js/renderLibrary.js
+++ src/js/renderLibrary.js
@@ -1,5 +1,29 @@
-// get access to the My Library button
-// add event listener on the My Library button
+// arrange library.html as hbs file
+// done
+
+// import refs
+import refs from './refs';
+// import hbs
+import mylibrary from '../templates/mylibrary.hbs';
+import handleAuthModals from './handle-authentication-modals';
+// get access to the My Library button, main
+
+const { mainHTML, myLibraryLink, homeLink } = refs;
+console.log(mainHTML, myLibraryLink, homeLink);
+
+// add event listener on My Library
+myLibraryLink.addEventListener('click', renderLibrary);
+
 // by click on the button:
-// 1. hide body content
-// 2. render mylinrary html
+function renderLibrary() {
+  // 1. hide main content
+  mainHTML.innerHTML = '';
+  // 2. render content from mylibrary.hbs
+  const libraryMarkup = mylibrary();
+  mainHTML.insertAdjacentHTML('beforeend', libraryMarkup);
+  // 3. handle modals
+  handleAuthModals();
+  // 4. switch current page style
+  homeLink.classList.remove('current');
+  myLibraryLink.classList.add('current');
+}
