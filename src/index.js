0f2a1d6f05922115bd921eab357b7983a1751100
AleksandrOnis <sashaonis@gmail.com>
2021-10-14 09:46:39 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './sass/main.scss';
-import filmСard from './templates/filmСard.hbs';
-import filmDescription from './templates/filmDescription.hbs';
-import filmsApiService from './js/apiService.js';
-import Notiflix from 'notiflix';
 import './js/modalFooter.js';
 import './js/renderHome.js';
 import './js/renderSearchMovie.js';
