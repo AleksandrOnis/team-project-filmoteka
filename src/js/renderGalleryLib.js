79493856e512e08ce11ae723ac8b194a0863edae
MarharytaBoichenko <boichenkomv@gmail.com>
2021-10-20 12:42:43 +0300

    save

--- src/js/renderGalleryLib.js
+++ src/js/renderGalleryLib.js
@@ -1,5 +1,6 @@
 import filmCard from '../templates/filmcard-lib.hbs';
 import { createFilmCard } from './renderHome';
+import { showSpiner, hideSpiner } from './spiner.js';
 
 export function renderGalleryLib() {
   const btnsLibRef = document.querySelector('.library-header__buttons__wrapper');
@@ -11,27 +12,38 @@ export function renderGalleryLib() {
   const galleryLib = document.querySelector('.watched-list');
   
   function renderWatched() {
-      const films = getWatchedFromLocalStorage();
-      const genres = films.flatMap(film => film.genres); //.filter(); //unik
-      // console.log('🚀 ~ renderWatched ~ genres', genres);
-      // const filmCard = createFilmCard(films, genres); //ошибка по жанрам
+    showSpiner();
+    const films = getWatchedFromLocalStorage();
+    console.log(films)
+    if (films.length === 0) {
+      return
+    }
+    const genres = films.flatMap(film => film.genres); //.filter(); //unik
+    // console.log('🚀 ~ renderWatched ~ genres', genres);
+    // const filmCard = createFilmCard(films, genres); //ошибка по жанрам
     renderFilmCard(films);
-      if (galleryLib.classList.contains('is-hidden')) {
+    if (galleryLib.classList.contains('is-hidden')) {
       galleryLib.classList.remove('is-hidden')
     }
-  
+  hideSpiner();
   }
 
   function renderQueue() {
-     galleryLib.classList.toggle('is-hidden')
+     showSpiner();
+    //  galleryLib.classList.toggle('is-hidden')
     const films = getQueueFromLocalStorage();
+     if (films.length === 0) {
+      return
+    }
+    console.log(films)
     const genres = films.flatMap(film => film.genres); //.filter(); //unik
     // console.log('🚀 ~ renderWatched ~ genres', genres);
     // const filmCard = createFilmCard(films, genres); //ошибка по жанрам
     renderFilmCard(films);
      if (galleryLib.classList.contains('is-hidden')) {
       galleryLib.classList.remove('is-hidden')
-    }
+     }
+    hideSpiner();
   }
 
   function renderFilmCard(films = 0) {
